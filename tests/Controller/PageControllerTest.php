bd892bf3e7b4bdc2a5f2c98ca35678bfb624e224
Emre Kucuk <kucukemre96@gmail.com>
2018-02-23 13:25:25 +0300

    phpunit file path changed

---
+++ tests/Controller/PageControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace OCA\Analysis_App\Tests\Controller;
+
+use OCA\Analysis_App\Controller\PageController;
+use OCP\IRequest;
+use Test\TestCase;
+
+class PageControllerTest extends TestCase {
+    public function testTrueIsTrue() {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
