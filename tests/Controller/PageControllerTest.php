72c9da015de1dca183bf813fd8a518755a372677
Emre Kucuk <kucukemre96@gmail.com>
2018-03-08 21:32:24 +0300

    pagecontroller unit test

--- tests/Controller/PageControllerTest.php
+++ tests/Controller/PageControllerTest.php
@@ -2,13 +2,45 @@
 
 namespace OCA\Analysis_App\Tests\Controller;
 
-use OCA\Analysis_App\Controller\PageController;
-use OCP\IRequest;
+use OCA\Analysis_app\Controller\PageController;
+use OCA\Analysis_app\FileModel;
+use OCP\AppFramework\Http\TemplateResponse;
 use Test\TestCase;
 
+
 class PageControllerTest extends TestCase {
-    public function testTrueIsTrue() {
-        $foo = true;
-        $this->assertTrue($foo);
+    /**
+     * @params $appName
+     * @params $fileModel
+     * @params $request
+     */
+    private $appName;
+    private $fileModel;
+    private $request;
+    private $expectedHtml;
+
+
+    protected function setUp() {
+        parent::setUp();
+        $this->appName = $this->createMock('\OCA\Analysis_app\Controller\PageController');
+        $this->request = $this->createMock('\OCP\IRequest');
+        $this->fileModel = $this->createMock('\OCA\Analysis_app\FileModel');
+        $this->expectedHtml = $this->getMockBuilder('OCP\AppFramework\Http\TemplateResponse')
+            ->disableOriginalConstructor()
+            ->setConstructorArgs(array($this->appName, 'index'));
+    }
+
+    public function testIndex() {
+
+        $templateObj = new PageController($this->appName, $this->request, $this->fileModel);
+        $this->assertTemplate($this->expectedHtml, $templateObj->index());
+
+
+
+
+    }
+
+    public function testGetInfo() {
+
     }
 }
