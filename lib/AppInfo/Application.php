a11ab01898b1b027be05261b6b507c9fac76cc3d
karakayasemi <karakayasemi@itu.edu.tr>
2018-03-19 10:48:43 +0300

    inject user folder to filemodel

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -1,12 +1,27 @@
 <?php
 namespace OCA\Analysis_app\AppInfo;
+use OCA\Analysis_app\Controller\PageController;
+use OCA\Analysis_app\FileModel;
 use \OCP\AppFramework\App;
-use OCP\IContainer;
 
 class Application extends App {
-    //Define your dependencies here
-    public function __construct(array $urlParams = array()) {
-        parent::__construct('analysis_app', $urlParams);
-        $container = $this->getContainer();
-    }
+	//Define your dependencies here
+	public function __construct(array $urlParams = array()) {
+		parent::__construct('analysis_app', $urlParams);
+		$container = $this->getContainer();
+
+		$container->registerService('FileModel', function($c) {
+			return new FileModel(
+				$c->query('ServerContainer')->getUserFolder()
+			);
+		});
+
+		$container->registerService('PageController', function($c) {
+			return new PageController(
+				$c->query('AppName'),
+				$c->query('Request'),
+				$c->query('FileModel')
+			);
+		});
+	}
 }
--- lib/FileModel.php
+++ lib/FileModel.php
@@ -6,80 +6,88 @@ use OCP\Files\Folder;
 use OCP\Files\Node;
 
 class FileModel {
-    /**
-     * @var Node[] $biggestFileNodes
-     */
-    private $biggestFileNodes;
+	/**
+	 * @var Node[] $biggestFileNodes
+	 */
+	private $biggestFileNodes;
 
-    /**
-     * @var array $mimeTypes
-     */
-    private $mimeTypes;
+	/**
+	 * @var array $mimeTypes
+	 */
+	private $mimeTypes;
 
-    /**
-     * @var array $biggestFiles
-     */
-    private $biggestFiles;
+	/**
+	 * @var array $biggestFiles
+	 */
+	private $biggestFiles;
 
-    /**
-     * FileModel constructor.
-     */
-    public function __construct() {
-        $this->analyze(\OC::$server->getUserFolder());
-    }
-    /**
-     * @param Folder $currentDirectory
-     */
-    public function analyze($currentDirectory) {
-        foreach ($currentDirectory->search('') as $item) {
-            if ($item instanceof Folder) {
-                continue;
-            }
-            $this->pushToBiggestFilesIfNecessary($item);
-            $this->analyzeMimeType($item);
-        }
-        $this->convertBiggestFilesNodeToArray();
-    }
-    /**
-     * @return array
-     */
-    public function getAnalysisReport() {
-        return [
-            'biggest_files' => $this->biggestFiles,
-            'mime_types' => $this->mimeTypes
-        ];
-    }
+	/**
+	 * @var Folder $userFolder
+	 */
+	private $userFolder;
 
-    /**
-     * @param Node $item
-     */
-    public function pushToBiggestFilesIfNecessary($item) {
-        if (count($this->biggestFileNodes) < 10
-            || $item->getSize() > $this->biggestFileNodes[9]->getSize()
-        ) {
-            $this->biggestFileNodes[9] = $item;
-            usort($this->biggestFileNodes, function (Node $a, Node $b) {
-                return $a->getSize() < $b->getSize();
-            });
-        }
-    }
+	/**
+	 * FileModel constructor.
+	 *
+	 * @param Folder $userFolder
+	 */
+	public function __construct($userFolder) {
+		$this->userFolder = $userFolder;
+		$this->analyze($userFolder);
+	}
+	/**
+	 * @param Folder $currentDirectory
+	 */
+	public function analyze($currentDirectory) {
+		foreach ($currentDirectory->search('') as $item) {
+			if ($item instanceof Folder) {
+				continue;
+			}
+			$this->pushToBiggestFilesIfNecessary($item);
+			$this->analyzeMimeType($item);
+		}
+		$this->convertBiggestFilesNodeToArray();
+	}
+	/**
+	 * @return array
+	 */
+	public function getAnalysisReport() {
+		return [
+			'biggest_files' => $this->biggestFiles,
+			'mime_types' => $this->mimeTypes
+		];
+	}
 
-    public function convertBiggestFilesNodeToArray() {
-        foreach ($this->biggestFileNodes as $node) {
-            $this->biggestFiles[$node->getName()] = $node->getSize();
-        }
-        arsort($this->biggestFiles);
-    }
+	/**
+	 * @param Node $item
+	 */
+	public function pushToBiggestFilesIfNecessary($item) {
+		if (count($this->biggestFileNodes) < 10
+			|| $item->getSize() > $this->biggestFileNodes[9]->getSize()
+		) {
+			$this->biggestFileNodes[9] = $item;
+			usort($this->biggestFileNodes, function (Node $a, Node $b) {
+				return $a->getSize() < $b->getSize();
+			});
+		}
+	}
 
-    /**
-     * @param Node $item
-     */
-    public function analyzeMimeType($item)
-    {
-        if (!isset($this->mimeTypes[$item->getMimetype()])) {
-            $this->mimeTypes[$item->getMimetype()] = $item->getSize();
-        } else {
-            $this->mimeTypes[$item->getMimetype()] += $item->getSize();
-        }
-    }
+	public function convertBiggestFilesNodeToArray() {
+		foreach ($this->biggestFileNodes as $node) {
+			$this->biggestFiles[$node->getName()] = $node->getSize();
+		}
+		arsort($this->biggestFiles);
+	}
+
+	/**
+	 * @param Node $item
+	 */
+	public function analyzeMimeType($item)
+	{
+		if (!isset($this->mimeTypes[$item->getMimetype()])) {
+			$this->mimeTypes[$item->getMimetype()] = $item->getSize();
+		} else {
+			$this->mimeTypes[$item->getMimetype()] += $item->getSize();
+		}
+	}
 }
