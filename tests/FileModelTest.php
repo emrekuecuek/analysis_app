9eb73c61fe423b84da0e6d2919cc052c2a756591
Emre Kucuk <kucukemre96@gmail.com>
2018-03-23 16:25:18 +0300

    file model test

--- tests/FileModelTest.php
+++ tests/FileModelTest.php
@@ -4,21 +4,31 @@ namespace OCA\Analysis_App\Tests;
 
 use OCA\Analysis_app\FileModel;
 use OCP\Files\Folder;
+use OCP\Files\Node;
 use Test\TestCase;
 
 class FileModelTest extends TestCase {
-    /** @var $currentFolderMock | \PHPUnit_Framework_MockObject_MockObject */
+    /** @var Folder $currentFolderMock | \PHPUnit_Framework_MockObject_MockObject */
     private $currentFolderMock;
     /** @var FileModel $fileModelObject */
     private $fileModelObject;
+    /** @var Node | \PHPUnit_Framework_MockObject_MockObject */
+    private $nodeMock1;
+    /** @var Node | \PHPUnit_Framework_MockObject_MockObject */
+    private $nodeMock2;
 
     protected function setUp() {
         parent::setUp();
         $this->currentFolderMock = $this->createMock(Folder::class);
+        $this->nodeMock1 = $this->createMock(Node::class)
+            ->expects($this->once())
+            ->method('getName')->willReturn('nodeMock1 test Name')
+            ->method('getSize')->willReturn(10)
+            ->method('getMimeType')->willReturn('testMimeType');
+        $this->nodeMock2 = $this->createMock(Node::class)
+            ->expects($this->once())
+            ->method('getName')->willReturn('nodeMock2 test Name')
+            ->method('getSize')->willReturn(20);
         $this->fileModelObject = new FileModel($this->currentFolderMock);
-    }
-
-    public function testGetAnalysisReport() {
-        $this->assertArrayHasKey(array('biggest_files','mime_types'),$this->fileModelObject->getAnalysisReport());
     }
 }
